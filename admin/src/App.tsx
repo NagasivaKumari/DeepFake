0d0c42c782dfb34196564cd6c3cd32842d405d86
Nagasiva Kumari Kota <nagashivakota@gmail.com>
2025-11-06 19:27:05 +0530

    feat(admin): update App to support registration & admin flows

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import AppLayout from "./AppLayout";
 import KycAdmin from "./KycAdmin";
 import Users from "./Users";
 import Settings from "./Settings";
+import MediaAdmin from "./MediaAdmin";
+
 
 const App = () => (
 	<BrowserRouter>
 		<AppLayout>
 			<Routes>
 				<Route path="/" element={<Users />} />
+				<Route path="/media" element={<MediaAdmin />} />
 				<Route path="/settings" element={<Settings />} />
 			</Routes>
 		</AppLayout>
