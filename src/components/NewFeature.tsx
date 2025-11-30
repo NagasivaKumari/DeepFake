d5fa9878a5dc993435f7f327c9e49f0a4c4955fe
Nagasiva Kumari Kota <nagashivakota@gmail.com>
2025-11-30 23:41:06 +0530

    Add click handler to button in NewFeature component

--- src/components/NewFeature.tsx
+++ src/components/NewFeature.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 const NewFeature = () => {
+    const handleClick = () => {
+        alert('Button clicked!');
+    };
+
     return (
         <div>
             <h1>New Feature Component</h1>
             <p>This is a placeholder for the new feature.</p>
-            <button>Click Me</button>
+            <button onClick={handleClick}>Click Me</button>
         </div>
     );
 };
