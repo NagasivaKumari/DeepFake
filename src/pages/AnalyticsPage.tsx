1179f0ebaf7bfe5789d77201412787e442a532f8
Nagasiva Kumari Kota <nagashivakota@gmail.com>
2025-11-30 23:21:20 +0530

    Create AnalyticsPage to integrate AnalyticsDashboard

---
+++ src/pages/AnalyticsPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AnalyticsDashboard from '../components/AnalyticsDashboard';
+
+const AnalyticsPage: React.FC = () => {
+  return (
+    <div className="analytics-page">
+      <AnalyticsDashboard />
+    </div>
+  );
+};
+
+export default AnalyticsPage;
